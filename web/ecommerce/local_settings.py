3ad7109abdab9773c841063005895ee2a3d4d41a
Akin <adekoya@gmail.com>
2019-01-14 17:28:58 -0500

    Now on Docker

--- web/ecommerce/local_settings.py
+++ web/ecommerce/local_settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'oq*rcu%@n0&zakgphd%kbrz-f4t6!k)ut0^%k^4#in&r!e=r2d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '.incertotech.com', '35.175.177.130']
 
 
 # Application definition
@@ -78,12 +78,18 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+    'postgresql': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecommerce',
+        'USER': 'django',
+        'PASSWORD': 'pwordyaezz',
+        'HOST': 'postgres',
+        'PORT': 5432,
     }
 }
 
+DATABASES['default'] = DATABASES['postgresql']
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -119,23 +125,30 @@ USE_TZ = True
 
 USE_THOUSAND_SEPARATOR = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+# Static files Settings (CSS, JavaScript, Images)
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
+# Additional locations of static files
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')),
+
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+# File Uploads Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'shop:product_list'
 LOGOUT_REDIRECT_URL = 'shop:product_list'
 LOGIN_URL = 'shop:login'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = "media"
+#MEDIA_ROOT = "media"
 
 CART_SESSION_ID = 'cart'
 
-try:
-    from .local_settings import *
-except ImportError:
-    raise Exception("A local_settings.py file is required to run this project")
